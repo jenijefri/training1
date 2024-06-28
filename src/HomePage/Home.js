31538264a92371394c3da7edb75f5e721884470b
jefrinasujil <jenijefri.sujil@gmail.com>
2024-06-28 22:50:53 +0530

    Home page code changes commit

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -12,6 +12,8 @@ import { FaGift } from "react-icons/fa";
 import { IoMdHelpCircle } from "react-icons/io";
 import { FaFacebookSquare } from "react-icons/fa";
 
+// Ensure you import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   const imageLinks = [
@@ -22,13 +24,16 @@ export default function Home() {
     "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/3a78fc043a9f7531.jpg?q=20",
     "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/5d96318f16c153aa.jpeg?q=20",
   ];
+
   const dataDisplay = datalist.slice(0, 4);
   const dataDisplayTwo = datalist.slice(12, 20);
+
   return (
     <>
       <Navbar />
-      
+
       <div className="category">
+        {/* Category items */}
         <div className="category-items">
           <img
             alt="Grocery"
@@ -88,24 +93,24 @@ export default function Home() {
         </div>
         <div className="category-items">
           <img
-            alt="Beauty, Toys &amp; More"
+            alt="Beauty, Toys & More"
             src="https://rukminim1.flixcart.com/flap/96/96/image/dff3f7adcf3a90c6.png?q=100"
           ></img>
           <p>Toys</p>
         </div>
       </div>
 
-      <Carousel indicators={false} className="carousel">
-      {imageLinks.map((imageLink, index) => (
-        <Carousel.Item key={index}>
-          <img
-            className="d-block w-100 custom-img"
-            src={imageLink}
-            alt={`Slide ${index + 1}`}
-          />
-        </Carousel.Item>
-      ))}
-    </Carousel>
+      <Carousel indicators={true} className="carousel">
+        {imageLinks.map((imageLink, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100 custom-img"
+              src={imageLink}
+              alt={`Slide ${index + 1}`}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
       <h2>
         Recommended <FaCircleChevronRight className="left-arrow" />
@@ -126,9 +131,9 @@ export default function Home() {
       <h2>
         New Products <FaCircleChevronRight className="left-arrow" />
       </h2>
-      <div className="items-container" style={{justifyContent:'space-evenly'}}>
+      <div className="items-container" style={{ justifyContent: 'space-evenly' }}>
         {dataDisplayTwo.map((item, index) => (
-          <div key={index} id={item.id} className="items-home" style={{margin:'5px'}}>
+          <div key={index} id={item.id} className="items-home" style={{ margin: '5px' }}>
             <img src={item.img} alt={item.title} className="products" />
             <p className="title">{item.title}</p>
             <Link to={"/Product"}>
@@ -175,52 +180,49 @@ export default function Home() {
             <h4>Connect with Us</h4>
             <ul>
               <li>
-                <FaFacebookSquare style={{height:'18px',width:'18px',marginRight:'5px'}} /> Facebook
+                <FaFacebookSquare style={{ height: '18px', width: '18px', marginRight: '5px' }} /> Facebook
               </li>
               <li>
-                <BsTwitterX style={{height:'16px',width:'16px',marginRight:'5px'}}/> Twitter
+                <BsTwitterX style={{ height: '16px', width: '16px', marginRight: '5px' }} /> Twitter
               </li>
               <li>
-                <SiInstagram style={{height:'16px',width:'16px',marginRight:'5px'}}/> Instagram
+                <SiInstagram style={{ height: '16px', width: '16px', marginRight: '5px' }} /> Instagram
               </li>
             </ul>
           </div>
         </div>
-        
 
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center', borderTop: '1px solid #fff',padding: '15px' }}>
-  <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-    <FaShopLock style={{ color: '#FFC200' }} />
-    <span style={{ marginLeft: '5px' }}>Become a Seller</span>
-  </div>
-
-  <div className="c4gehN" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-    <RiAdvertisementFill style={{ color: '#FFC200' }} />
-    <span style={{ marginLeft: '5px' }}>Advertise</span>
-  </div>
-
-  <div className="c4gehN" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-    <FaGift style={{ color: '#FFC200' }} />
-    <span style={{ marginLeft: '5px' }}>Gift Cards</span>
-  </div>
-
-  <div className="c4gehN" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-    <IoMdHelpCircle style={{ color: '#FFC200' }} />
-    <span style={{ marginLeft: '5px' }}>Help Center</span>
-  </div>
-</div>
-
-
-
-        <div style={{display:'flex',justifyContent:'center',width:'100%',padding:'10px'}}>
-            <img
-              src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/payment-method-c454fb.svg"
-              alt="Payment methods"
-              style={{width:'100%'}}
-            />
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center', borderTop: '1px solid #fff', padding: '15px' }}>
+          <div style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <FaShopLock style={{ color: '#FFC200' }} />
+            <span style={{ marginLeft: '5px' }}>Become a Seller</span>
           </div>
+
+          <div className="c4gehN" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <RiAdvertisementFill style={{ color: '#FFC200' }} />
+            <span style={{ marginLeft: '5px' }}>Advertise</span>
+          </div>
+
+          <div className="c4gehN" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <FaGift style={{ color: '#FFC200' }} />
+            <span style={{ marginLeft: '5px' }}>Gift Cards</span>
+          </div>
+
+          <div className="c4gehN" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <IoMdHelpCircle style={{ color: '#FFC200' }} />
+            <span style={{ marginLeft: '5px' }}>Help Center</span>
+          </div>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%', padding: '10px' }}>
+          <img
+            src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/payment-method-c454fb.svg"
+            alt="Payment methods"
+            style={{ width: '100%' }}
+          />
+        </div>
         <div className="footer-bottom">
-          <p>&copy; 2023 Your E-Commerce Store. All Rights Reserved. Build with ❤️ by <Link style={{textDecoration:'none',color:'#FFC200'}} to={"https://github.com/ViNu-23"}>ViNu23</Link></p>
+          <p>&copy; 2023 Your E-Commerce Store. All Rights Reserved. Build with ❤️ by <Link style={{ textDecoration: 'none', color: '#FFC200' }} to={"https://github.com/ViNu-23"}>ViNu23</Link></p>
         </div>
       </footer>
     </>
